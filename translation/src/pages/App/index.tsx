4e6fa654feafb5c32a523dc527b139d96782b65b
taiki-fw <taiki_52@outlook.jp>
2021-01-12 22:11:00 +0900

    :tada:translation initial commit

---
+++ translation/src/pages/App/index.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+export function App() {
+  return (
+    <div>
+      <header>
+        <h1>Default Page</h1>
+        <a href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
